d1c1456091ea46da8574cb36a35a4ec2f99823aa
lemendez-hub <lemendez@csumb.edu>
2026-02-03 20:22:23 -0800

    Fonts
    Created new legacy texts to be able to display on screen. Modified the ball script to be able to add text and to keep track of player score.

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallScript : MonoBehaviour
 {
@@ -7,30 +8,43 @@ public class BallScript : MonoBehaviour
     private int player1Score;
     private int player2Score;
     public Vector3 spawnPoint;
+    public Text player1Text;
+    public Text player2Text; 
     
     void Start()
     {
+        player1Score = 0;
+        player2Score = 0;
         direction = new Vector3(1f, 0f, 1f);
-        }
+    }
     
     void Update()
     {
         transform.position = transform.position + direction * speed * Time.deltaTime;
+        player1Text.text = player1Score.ToString();
+        player2Text.text = player2Score.ToString();
     }
     
     void OnCollisionEnter(Collision collision)
     {
         Vector3 normalHit = collision.contacts[0].normal;
         direction = Vector3.Reflect(direction, normalHit);
+        
         if(collision.gameObject.name == "West")
         {
             player2Score++;
-            transform.position = spawnPoint;
+            ResetBall();
         }
         if(collision.gameObject.name == "East")
         {
             player1Score++;
-            transform.position = spawnPoint;
+            ResetBall();
         }
+    }
+
+    void ResetBall()
+    {
+        transform.position = spawnPoint;
+        direction = new Vector3(1f, 0f, 1f);
     }
 }
