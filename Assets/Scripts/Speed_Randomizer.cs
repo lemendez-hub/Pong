7b2e8b1a4e23f55b4e2cd754e44ffa9238703eca
lemendez-hub <lemendez@csumb.edu>
2026-02-07 15:44:43 -0800

    New Materials
    Created new materials for the power ups and gave them some PNG.

---
+++ Assets/Scripts/Speed_Randomizer.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Speed_Randomizer : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float maxSpeed = 40f;
+    public float minspeed = 1f;
+    public float seconds = 15f;
+    public float maxSeconds = 7f;
+    private float timer;
+
+
+    void Start()
+    {
+        SpawnCube();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= seconds)
+        {
+            SpawnCube();
+            timer = 0f;
+        }
+    }
+
+    void SpawnCube()
+    {
+        float randomX = Random.Range(-40.0f, 40.0f);
+        float randomZ = Random.Range(-20.0f, 20.0f);
+        Vector3 spawn = new Vector3(randomX, 1f, randomZ);
+        GameObject ball = Instantiate(ballPrefab, spawn, Quaternion.identity);
+        Destroy(ball, maxSeconds);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        BallScript ballScript = other.gameObject.GetComponent<BallScript>();
+        if (ballScript != null)
+        {
+            float randomSpeed = Random.Range(minspeed, maxSpeed);
+            ballScript.speed = randomSpeed;
+            Destroy(gameObject);
+            Debug.Log("Ball speed has been randomized to: " + randomSpeed);
+        }
+        {
+            
+        }
+    }
+}
