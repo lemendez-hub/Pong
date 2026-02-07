0affa3e885048535525a29a36ecc63e237326ad2
lemendez-hub <lemendez@csumb.edu>
2026-02-07 05:13:32 -0800

    Power Ups Draft
    Created game objects and scripts that allow me to spawn power ups at the start and then disappear/spawn them again after certain seconds. Just need to actually implement the power ups and what they do.

---
+++ Assets/Scripts/Score_Reduction.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Score_Reduction : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float seconds = 15f;
+    public float maxSeconds = 7f;
+    private float timer;
+
+    void Start()
+    {
+        SpawnCube();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= seconds)
+        {
+            SpawnCube();
+            timer = 0f;
+        }
+    }
+
+    void SpawnCube()
+    {
+        float randomX = Random.Range(-22.0f, 22.0f);
+        float randomZ = Random.Range(-20.0f, 20.0f);
+        Vector3 spawn = new Vector3(randomX, 1f, randomZ);
+        GameObject cube = Instantiate(cubePrefab, spawn, Quaternion.identity);
+        Destroy(cube, maxSeconds);
+    }
+}
